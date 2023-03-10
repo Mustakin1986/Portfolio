37154a4e0c077b0298f62cb6cccc658c248c3288
Mustakin1986 <mustakinali@gmail.com>
2023-03-10 22:26:45 +0600

    first

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\Backend\BackendController;
+use App\Http\Controllers\Frontend\FrontendController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+//admin Route
+Route::get('/admin/login',[BackendController::class,'login']);
+
+
+Route::get('/',[FrontendController::class,'index']);
