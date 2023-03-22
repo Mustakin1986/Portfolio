8036a61376cbdbb752313aa80684a88e2f9d9c0c
Mustakin1986 <111695654+Mustakin1986@users.noreply.github.com>
2023-03-22 16:26:06 +0600

    update Route

--- routes/web.php
+++ routes/web.php
@@ -27,18 +27,37 @@ use App\Http\Controllers\Frontend\ContactformController;
    Route::group(['middleware'=>'auth', 'prefix'=>'dashboard'], function (){
    Route::get('/', [BackendController::class, 'index'])->name('back.index');
 
-     //MenuBar
-     
-   //service section end
+ //MenuBar
+ Route::get('/menus/create',[MenuController::class,'create'])->name('menus.create');
+ Route::get('/menus',[MenuController::class,'index'])->name('menus.index');
+ Route::post('/menus/store',[MenuController::class,'Store'])->name('menus.store');
+ Route::get('/menus/delete/{id}',[MenuController::class,'Delete'])->name('menu.delete');
+ Route::get('/menus/{id}/edit',[MenuController::class,'Edit'])->name('menus.edit');
+ Route::post('/menus/update/{id}',[MenuController::class,'menuUpdate'])->name('menus.update');
 
+ //Slider Controller
+ Route::get('/sliders',[SliderController::class,'index'])->name('sliders.index');
+ Route::get('/sliders/create',[SliderController::class,'create'])->name('sliders.create');
+ Route::post('/sliders/store',[SliderController::class,'store'])->name('sliders.store');
+ Route::get('/sliders/{id}/edit',[SliderController::class,'edit'])->name('sliders.edit');
+ Route::put('/sliders/{id}',[SliderController::class,'update'])->name('sliders.update');
+//Service Controller
+Route::get('/service/create',[ServiceController::class,'serviceCreate'])->name('service.create');
+Route::post('/service/store',[ServiceController::class,'serviceStore'])->name('service.store');
+Route::get('/service/list',[ServiceController::class,'serviceList'])->name('service.list');
+Route::get('/service/delete/{id}',[ServiceController::class,'serviceDelete'])->name('service.delete');
+Route::get('/service/edit/{id}',[ServiceController::class,'serviceEdit'])->name('service.edit');
+Route::post('/service/update/{id}',[ServiceController::class,'serviceUpdate'])->name('service.update');
+
+ });
+ 
    //CONTACT FORM START
-    Route::post('contactForm/create',[ContactformController::class,'contactFormCreate'])->name('contactForm.create');
+   Route::post('contactForm/create',[ContactformController::class,'contactFormCreate'])->name('contactForm.create');
     
    Route::get('contact/list',[ContactformController::class,'contactList'])->name('contact.list');
 
    //CONTACT FORM END
 
- });
 
 Auth::routes();
 
